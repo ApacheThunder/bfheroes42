7320916b3a1ed9cf8d189baa8b00152c6be28b74
ApacheThunder <spenserwhaley@gmail.com>
2016-09-12 14:08:46 -0500

    Initial Commit 16

---
+++ bf1942/levels/Wicked_Wake/WaterShader.rs
@@ -0,0 +1,9 @@
+subshader "WaterElala" "PatchTerrain/Water"
+{
+	lighting true;
+	lightingSpecular true;
+	materialDiffuse 1 1 1;
+	materialSpecular 1 1 1;
+	materialSpecularPower 50;
+	envmap true;
+}
