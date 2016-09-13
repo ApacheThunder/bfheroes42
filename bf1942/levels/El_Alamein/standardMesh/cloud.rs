81e66cc09c7cab696e041088d2d64fcb20c1e5a1
ApacheThunder <spenserwhaley@gmail.com>
2016-09-12 20:01:16 -0500

    Final Initial Commit

---
+++ bf1942/levels/El_Alamein/standardMesh/cloud.rs
@@ -0,0 +1,8 @@
+subshader "Cloud0" "StandardMesh/SkyBox"
+{
+	lighting false;
+	lightingSpecular false;
+	materialDiffuse 1 1 1;
+	materialAmbient 1 1 1;
+	texture "texture/cloudDust1";
+}
---
+++ bf1942/levels/El_Alamein_Nights/standardMesh/cloud.rs
@@ -0,0 +1,7 @@
+subshader "cloud_Material0" "StandardMesh/Default"
+{
+	lighting false;
+	lightingSpecular false;
+	materialDiffuse 1 1 1;
+	texture "texture/cloud";
+}
